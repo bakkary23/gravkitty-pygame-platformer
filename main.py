cdd5c8ae8d7258f73b9c1f3f0801070d1b26e93a
bakkary <bakkary23@gmail.com>
2022-05-20 14:48:50 -0700

    Added music, states

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ pygame.display.set_icon(pygame.image.load('graphics/player.png').convert_alpha()
 pygame.display.set_caption('Gravkitty')
 clock = pygame.time.Clock()
 game_active = False
+mus = 0
+stop = 0
+title_music = pygame.mixer.Sound('audio/titletheme.mp3')
+title_music.set_volume(0.1)
 game_music = pygame.mixer.Sound('audio/gamemusic.mp3')
 game_music.set_volume(0.1)
-game_music.play(loops=-1)
 
 
 def surfaces(screen):
@@ -24,14 +27,30 @@ def surfaces(screen):
     screen.blit(ceiling_surface, (0, 0))
 
 
-# def music(state):
-#     title_music = pygame.mixer.Sound('audio/titletheme.mp3')
-#     game_music = pygame.mixer.Sound('audio/gamemusic.mp3')
-#     if state is False:
-#         title_music.play()
-#     keys = pygame.key.get_pressed()
-#     if keys[pygame.K_RETURN]:
-#         game_music.play(loops=-1)
+def game_over(screen):
+    font = pygame.font.Font('freesansbold.ttf', 100)
+    game_over_message = font.render('GAME OVER', False, 'red')
+    game_over_message_rect = game_over_message.get_rect(center=(500, 250))
+    font2 = pygame.font.Font('freesansbold.ttf', 32)
+    restart_message = font2.render('Press space to exit', False, 'red')
+    restart_message_rect = restart_message.get_rect(center=(500, 400))
+    title_background = pygame.image.load('graphics/stars.png').convert_alpha()
+    screen.blit(title_background, (0, 0))
+    screen.blit(game_over_message, game_over_message_rect)
+    screen.blit(restart_message, restart_message_rect)
+
+
+def win_screen(screen):
+    font = pygame.font.Font('freesansbold.ttf', 100)
+    win_message = font.render('YOU WIN!', False, 'green')
+    win_message_rect = win_message.get_rect(center=(500, 250))
+    font2 = pygame.font.Font('freesansbold.ttf', 32)
+    restart_message = font2.render('Press space to exit', False, 'green')
+    restart_message_rect = restart_message.get_rect(center=(500, 400))
+    title_background = pygame.image.load('graphics/stars.png').convert_alpha()
+    screen.blit(title_background, (0, 0))
+    screen.blit(win_message, win_message_rect)
+    screen.blit(restart_message, restart_message_rect)
 
 
 def intro_screen(screen):
@@ -51,11 +70,18 @@ def intro_screen(screen):
     screen.blit(game_title, game_title_rect)
 
 
+def collisions():
+    if pygame.sprite.spritecollide(player.sprite, obstacles, False):
+        return 1
+    if pygame.sprite.spritecollide(player.sprite, end, False):
+        return 2
+
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load('graphics/player.png').convert_alpha()
-        self.rect = self.image.get_rect(topleft=(100, 470))
+        self.rect = self.image.get_rect(topleft=(100, 490))
         self.switchsound = pygame.mixer.Sound('audio/gravityswitch.mp3')
         self.gravity = 0
         self.direction = 1
@@ -100,7 +126,14 @@ class Player(pygame.sprite.Sprite):
             self.rect.top = 53
 
     def restart(self):
-        self.rect = self.image.get_rect(topleft=(100, 470))
+        self.rect = self.image.get_rect(topleft=(100, 490))
+        if self.direction == -1:
+            self.image = pygame.transform.flip(self.image, False, True)
+            self.gravity = 0
+            self.direction = 1
+        if self.flip == 0:
+            self.flip = 1
+            self.image = pygame.transform.flip(self.image, True, False)
 
     def update(self, state):
         """Function for adding player input to game loop"""
@@ -120,22 +153,24 @@ class Obstacle(pygame.sprite.Sprite):
             self.rect = self.image.get_rect(topleft=(310, 517))
         if types == 'spikes2':
             self.image = pygame.image.load('graphics/spikes.png').convert_alpha()
-            self.rect = self.image.get_rect(topleft=(750, 53))
+            self.rect = self.image.get_rect(topleft=(700, 53))
             self.image = pygame.transform.flip(self.image, False, True)
         if types == 'blackhole':
             self.image = pygame.image.load('graphics/blackhole.png').convert_alpha()
             self.rect = self.image.get_rect(topleft=(450, 250))
 
 
-def collisions():
-    if pygame.sprite.spritecollide(player.sprite, obstacles, False):
-        return False
-    else:
-        return True
+class End(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('graphics/end.png').convert_alpha()
+        self.rect = self.image.get_rect(topleft=(850, 458))
 
 
 player = pygame.sprite.GroupSingle()
 player.add(Player())
+end = pygame.sprite.GroupSingle()
+end.add(End())
 obstacles = pygame.sprite.Group()
 obstacles.add(Obstacle('spikes1'))
 obstacles.add(Obstacle('spikes2'))
@@ -151,12 +186,41 @@ while True:
             exit()
 
     if game_active:
+        if mus == 1:
+            title_music.stop()
+            game_music.play()
+            mus = 0
         surfaces(display)
         player.draw(display)
         obstacles.draw(display)
-        player.update(game_active)
-        game_active = collisions()
+        end.draw(display)
+        if stop != 1:
+            player.update(game_active)
+        if collisions() == 1:
+            game_music.stop()
+            game_over(display)
+            if stop == 0:
+                pygame.mixer.Sound('audio/win.mp3').play()
+            stop = 1
+            key = pygame.key.get_pressed()
+            if key[pygame.K_SPACE]:
+                stop = 0
+                game_active = False
+        if collisions() == 2:
+            game_music.stop()
+            win_screen(display)
+            if stop == 0:
+                pygame.mixer.Sound('audio/lose.mp3').play()
+            stop = 1
+            key = pygame.key.get_pressed()
+            if key[pygame.K_SPACE]:
+                stop = 0
+                game_active = False
     else:
+        if mus == 0:
+            game_music.stop()
+            title_music.play()
+            mus = 1
         intro_screen(display)
         key = pygame.key.get_pressed()
         if key[pygame.K_RETURN]:
